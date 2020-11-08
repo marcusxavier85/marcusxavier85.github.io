ae32d7ca41b641fff3d201c934ada864f4687eb1
Marcus Bowyer <marcusxavier85@gmail.com>
2020-11-08 13:45:49 +0000

    started working on making the portfolio a PWA

---
+++ portfolioJS.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('sw-cache').then((cache) => {
+            return cache.add('portfolio.html');
+            return cache.add('portstyle.css');
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    )
+})
